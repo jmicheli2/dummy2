1bee4207b173af1ebd549bcf1d28a9c255e1394c
JM <kmicheli2@gmail.col>
2020-11-11 16:04:49 +0100

    Initial Commit

---
+++ dummy2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dummy2
+//
+//  Created by Jacques Micheli on 11/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
